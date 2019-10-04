e841118ec42c1a465ff5f9e7a8aa0e853e959723
SUVOLewis <lewis.kosztan@suvotechnology.com>
2019-10-04 21:46:12 +0100

    Increased the map dimensions to the size it should be

--- crawler redo/crawler redo.cpp
+++ crawler redo/crawler redo.cpp
@@ -5,14 +5,14 @@ void genMap(char* outNum);
 
 int main()
 {
-	char myArray[5 * 3];
+	char myArray[31 * 10];
 	genMap(myArray);
 
-	for (int y = 0; y < 3; y++)
+	for (int y = 0; y < 10; y++)
 	{
-		for (int x = 0; x < 5; x++)
+		for (int x = 0; x < 31; x++)
 		{
-			std::cout << myArray[x + y * 5];
+			std::cout << myArray[x + y * 31];
 		}
 		std::cout << std::endl;
 	}
@@ -22,18 +22,18 @@ int main()
 
 void genMap(char* outNum)
 {
-	char array[5 * 3];
-	for (int y = 0; y < 3; y++)
+	char array[31 * 10];
+	for (int y = 0; y < 10; y++)
 	{
-		for (int x = 0; x < 5; x++)
+		for (int x = 0; x < 31; x++)
 		{
-			array[x + y * 5] = 'x';
-			std::cout << array[x + y * 5];
-			outNum[x + y * 5] = array[x + y * 5];
+			array[x + y * 31] = 'x';
+			std::cout << array[x + y * 31];
+			outNum[x + y * 31] = array[x + y * 31];
 		}
 
 		std::cout << std::endl;
 	}
-	std::cout << "--------------------" << std::endl;
+	std::cout << "--------------------------------------" << std::endl;
 	
 }
