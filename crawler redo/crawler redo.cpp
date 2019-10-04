72f29f3b4ba37c186488ac2f3a8619743c65d514
SUVOLewis <lewis.kosztan@suvotechnology.com>
2019-10-04 23:59:03 +0100

    Got the pointer passing properly. Will be moving onto player position detection next.

--- crawler redo/crawler redo.cpp
+++ crawler redo/crawler redo.cpp
@@ -1,11 +1,15 @@
 #include "pch.h"
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <iomanip>
+#include <string>
 
 void genMap(char* outNum);
 
 int main()
 {
-	char myArray[31 * 10];
+	char myArray[31 * 10]; // +11 To take into account the Null terminator, plus 10 lots of \n when getting the characters
 	genMap(myArray);
 
 	for (int y = 0; y < 10; y++)
@@ -23,17 +27,46 @@ int main()
 void genMap(char* outNum)
 {
 	char array[31 * 10];
-	for (int y = 0; y < 10; y++)
-	{
-		for (int x = 0; x < 31; x++)
-		{
-			array[x + y * 31] = 'x';
-			std::cout << array[x + y * 31];
-			outNum[x + y * 31] = array[x + y * 31];
-		}
+	//for (int y = 0; y < 10; y++)
+	//{
+	//	for (int x = 0; x < 31; x++)
+	//	{
+	//		array[x + y * 31] = 'x';
+	//		std::cout << array[x + y * 31];
+	//		outNum[x + y * 31] = array[x + y * 31];
+	//	}
 
-		std::cout << std::endl;
-	}
-	std::cout << "--------------------------------------" << std::endl;
+	//	std::cout << std::endl;
+	//}
+	//std::cout << "--------------------------------------" << std::endl;
 	
+	char theChar;
+	std::ifstream inFile;
+
+	inFile.open("./map.txt");
+	if (inFile.bad())
+	{
+		std::cerr << "Unable to open the map!";
+		exit(1);
+	}
+	else
+	{
+		int charNo = 0;
+		//while (inFile.get(theChar), !inFile.eof())
+		while (inFile >> theChar, !inFile.eof())
+		{
+			std::cout << theChar;
+			array[charNo] = theChar;
+			outNum[charNo] = array[charNo];
+			charNo++;
+
+			if (!(charNo % 31)) {
+				std::cout << std::endl;
+			}
+		}
+		std::cout << std::endl;
+		std::cout << "--------------------------------------" << std::endl;
+		std::cout << std::endl;
+		inFile.close();
+	}
 }
