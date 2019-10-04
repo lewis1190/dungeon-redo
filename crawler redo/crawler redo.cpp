dbb7220d4b763a0cc3ec5c2fa70061545318f096
SUVOLewis <lewis.kosztan@suvotechnology.com>
2019-10-04 21:39:34 +0100

    Got a proof of concept pointer thing working. About to move onto trying to apply the same principles to the map

--- crawler redo/crawler redo.cpp
+++ crawler redo/crawler redo.cpp
@@ -1,21 +1,39 @@
-// crawler redo.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include "pch.h"
 #include <iostream>
 
+void genMap(char* outNum);
+
 int main()
 {
-    std::cout << "Hello World!\n"; 
+	char myArray[5 * 3];
+	genMap(myArray);
+
+	for (int y = 0; y < 3; y++)
+	{
+		for (int x = 0; x < 5; x++)
+		{
+			std::cout << myArray[x + y * 5];
+		}
+		std::cout << std::endl;
+	}
+
+	std::cout << "Both grids should look the same";
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+void genMap(char* outNum)
+{
+	char array[5 * 3];
+	for (int y = 0; y < 3; y++)
+	{
+		for (int x = 0; x < 5; x++)
+		{
+			array[x + y * 5] = 'x';
+			std::cout << array[x + y * 5];
+			outNum[x + y * 5] = array[x + y * 5];
+		}
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+		std::cout << std::endl;
+	}
+	std::cout << "--------------------" << std::endl;
+	
+}
